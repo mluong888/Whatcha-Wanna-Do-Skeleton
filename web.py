062126ee3c6cedf1c3c43190b96f793059247a04
mluong888 <mluong888@berkeley.edu>
2019-10-20 06:15:47 -0400

    updates oops

--- web.py
+++ web.py
@@ -1,13 +1,17 @@
 from flask import Flask, request, render_template
+from app import eb_api_query
 import json
 
 app = Flask(__name__)
+
 
 @app.route('/form-example', methods=['GET', 'POST']) #allow both GET and POST requests
 def form_example():
     if request.method == 'POST':
         return;
-    return render_template('index.html')
+
+    # uncomment the line below after finishing the form-example demo    
+    # return render_template('index.html')
 
 
 if __name__ == '__main__':
